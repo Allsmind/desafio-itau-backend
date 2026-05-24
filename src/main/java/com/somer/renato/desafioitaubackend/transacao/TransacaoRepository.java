51f44a19c69660adf6d51ae6ddcbd94e1f5e1e28
Renato <somer.renato@gmail.com>
2026-05-24 19:51:43 -0300

    Adiciona endpoint e as classes necessarias para o registro das transações

---
+++ src/main/java/com/somer/renato/desafioitaubackend/transacao/TransacaoRepository.java
@@ -0,0 +1,23 @@
+package com.somer.renato.desafioitaubackend.transacao;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class TransacaoRepository {
+
+    private static final Logger log = LoggerFactory.getLogger(TransacaoRepository.class);
+    List<TransacaoRequest> listaDeTransacoes = new ArrayList<>();
+
+    //SALVA A TRANSACAO RECEBIDA NA LISTA
+    public void salvaTransacao(TransacaoRequest transacaoRequest) {
+        listaDeTransacoes.add(transacaoRequest);
+        log.info("Salvando transação");
+        log.info(Integer.toString(listaDeTransacoes.size()));
+        }
+
+}
